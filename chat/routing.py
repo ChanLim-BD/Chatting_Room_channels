7fe40eecd5769bea52e8305414d545b1edef78be
Chan_Lim <linsay070@gmail.com>
2025-02-10 10:06:30 +0900

    채팅방 단위로 그룹 채팅 및 회원만 접근 허용용

--- chat/routing.py
+++ chat/routing.py
@@ -3,5 +3,5 @@ from chat import consumers
 
 
 websocket_urlpatterns = [
-    path("ws/chat/<str:room_name>/chat/", consumers.ChatConsumer.as_asgi()),
+    path("ws/chat/<str:room_pk>/chat/", consumers.ChatConsumer.as_asgi()),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+
+from chat.forms import RoomForm
+from chat.models import Room
 
 def index(request):
-    return render(request, "chat/index.html")
+    room_qs = Room.objects.all()
+    return render(request, "chat/index.html", {
+        "room_list": room_qs,
+    })
 
-def room_chat(request, room_name):
+
+@login_required
+def room_new(request):
+    if request.method == "POST":
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            created_room = form.save()
+            return redirect("chat:room_chat", created_room.pk)
+    else:
+        form = RoomForm()
+    return render(request, "chat/room_form.html", {
+        "form": form,
+    })
+
+@login_required
+def room_chat(request, room_pk):
+    room = get_object_or_404(Room, pk=room_pk)
     return render(request, "chat/room_chat.html", {
-        "room_name": room_name,
+        "room": room,
     })
