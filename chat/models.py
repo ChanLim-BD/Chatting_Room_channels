462060e5c95ce52109dca56a2adacb5b66a16c1a
Chan_Lim <linsay070@gmail.com>
2025-02-10 12:52:50 +0900

    채팅방 삭제

--- chat/models.py
+++ chat/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from accounts.models import User 
 
 
 class Room(models.Model):
     name = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owned_room_set")
 
     class Meta:
         ordering = ["-pk"]
