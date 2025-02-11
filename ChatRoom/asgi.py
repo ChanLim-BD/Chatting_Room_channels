fe3f8af16fd5034963473fec768251d246cd723a
Chan_Lim <linsay070@gmail.com>
2025-02-11 10:53:39 +0900

    AllowedHostsOriginValidator 적용

--- ChatRoom/asgi.py
+++ ChatRoom/asgi.py
@@ -2,6 +2,7 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 import chat.routing
@@ -10,13 +11,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatRoom.settings')
 
 django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter(
-    {
-        "http": django_asgi_app,
-        "websocket": AuthMiddlewareStack( 
-                URLRouter(
-                chat.routing.websocket_urlpatterns,
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                chat.routing.websocket_urlpatterns
             )
-        ),
-    }
-)
+        )
+    ),
+})
